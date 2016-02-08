b75898aa3c1b23291625afce38e728160005ae31
Andrea Petreri <andrea.petreri@gmail.com>
2016-02-08 12:28:18 +0100

    Adding more events

--- calendar.go
+++ calendar.go
@@ -11,6 +11,9 @@ type Event struct {
 
 func GetEvents() []*Event {
 	events := make([]*Event, 0, 10)
-	events = append(events, &Event{Title:"first",Timestamp:time.Now()})
+	now := time.Now()
+	events = append(events, &Event{Title:"first",Timestamp:now.Add(time.Hour)})
+	events = append(events, &Event{Title:"second",Timestamp:now.Add(2*time.Hour)})
+	events = append(events, &Event{Title:"third",Timestamp:now.Add(3*time.Hour)})
 	return events
 }

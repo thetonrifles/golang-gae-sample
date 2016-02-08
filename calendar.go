a8b664417517692ebcc6e46aa739a398d60afdeb
Andrea Petreri <andrea.petreri@gmail.com>
2016-02-09 00:49:10 +0100

    Minor code update

--- calendar.go
+++ calendar.go
@@ -7,20 +7,40 @@ import (
     "google.golang.org/appengine/datastore"
 )
 
+type Calendar struct {
+  Id string       `json:"id"`
+  Owner string    `json:"owner"`
+  Events []*Event `json:"events"`
+}
+
 type Event struct {
-    Title string 		`json:"title"`
-    Timestamp time.Time `json:"timestamp"`
+  Title string 		    `json:"title"`
+  Timestamp time.Time `json:"timestamp"`
 }
 
-func GetEvents(r *http.Request) []Event {
+func GetCalendar(r *http.Request, id string) *Calendar {
   context := appengine.NewContext(r)
-  q := datastore.NewQuery("event")
-  var events []Event
-  q.GetAll(context, &events)
-	return events
+  q := datastore.NewQuery("calendar").Filter("Id=",id)
+  var calendar Calendar
+  keys, _ := q.GetAll(context, &calendar)
+  if len(keys) == 0 {
+    return nil
+  }
+	return &calendar
 }
 
-func PutEvent(r *http.Request, event Event) (bool, error) {
+func PostCalendar(r *http.Request, calendar Calendar) (bool, error) {
+  context := appengine.NewContext(r)
+  _, err := datastore.Put(context, datastore.NewIncompleteKey(context, "calendar", nil), &calendar)
+  if err != nil {
+    return false, err
+  } else {
+    return true, nil
+  }
+}
+
+/*
+func PutEvent(r *http.Request, calendarId string, event Event) (bool, error) {
   context := appengine.NewContext(r)
   _, err := datastore.Put(context, datastore.NewIncompleteKey(context, "event", nil), &event)
   if err != nil {
@@ -29,3 +49,4 @@ func PutEvent(r *http.Request, event Event) (bool, error) {
     return true, nil
   }
 }
+*/
--- main.go
+++ main.go
@@ -1,36 +1,61 @@
 package hw
 
 import (
-    "fmt"
-    "time"
-    "net/http"
-    "encoding/json"
+  "fmt"
+  "net/http"
+  "encoding/json"
+  "github.com/gorilla/mux"
 )
 
 type HttpResponse struct {
-    Status string   `json:"status"`
-    Message string  `json:"message"`
-    Entity Event    `json:"entity"`
+  Status string   `json:"status"`
+  Message string  `json:"message"`
 }
 
 func init() {
-    http.HandleFunc("/events", GetEventsHandler)
-    http.HandleFunc("/event", PostEventHandler)
+  r := mux.NewRouter()
+  r.HandleFunc("/calendar/{id}", GetCalendarHandler).Methods("GET")
+  r.HandleFunc("/calendar", PostCalendarHandler).Methods("POST")
+  http.Handle("/", r)
 }
 
-func GetEventsHandler(w http.ResponseWriter, r *http.Request) {
-	events := GetEvents(r)
-	json, _ := json.Marshal(events)
-  fmt.Fprint(w, string(json))
-}
-
-func PostEventHandler(w http.ResponseWriter, r *http.Request) {
-  event := Event{Title:"prova",Timestamp:time.Now()}
-  success, _ := PutEvent(r, event)
-  response := HttpResponse{Status:"success",Entity:event}
-  if !success {
-    response = HttpResponse{Status:"failure"}
+func GetCalendarHandler(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  calendar := GetCalendar(r, vars["id"])
+  if calendar != nil {
+    json, _ := json.Marshal(calendar)
+    fmt.Fprint(w, string(json))
+  } else {
+    errorHandler(w, r, http.StatusNotFound)
   }
-	json, _ := json.Marshal(response)
+}
+
+func PostCalendarHandler(w http.ResponseWriter, r *http.Request) {
+  decoder := json.NewDecoder(r.Body)
+  var calendar Calendar
+  err := decoder.Decode(&calendar)
+  if err == nil {
+    success, _ := PostCalendar(r, calendar)
+    if success {
+      json, _ := json.Marshal(calendar)
+      fmt.Fprint(w, string(json))
+    } else {
+      errorHandler(w, r, http.StatusInternalServerError)
+    }
+  } else {
+    errorHandler(w, r, http.StatusInternalServerError)
+  }
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+  w.WriteHeader(status)
+  var message string
+  if status == http.StatusNotFound {
+    message = "not found"
+  } else {
+    message = "bad request"
+  }
+  response := HttpResponse{Status:"failure",Message:message}
+  json, _ := json.Marshal(response)
   fmt.Fprint(w, string(json))
 }
